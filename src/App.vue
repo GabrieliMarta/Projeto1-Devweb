93900b4d0eb847a853ff6ddfbb1a988ce89825c9
gabrielimarta <gabrielirocha2730@gmail.com>
2024-07-05 12:42:48 -0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -71,19 +71,18 @@ function salvarPerfil() {
       <h1 class="title">{{ title }}</h1>
       <transition name="form" mode="out-in">
         <section v-if="mostrarPerfil">
-          <div class="mt-5 mb-3">
+          <div>
             <p v-for="(value, key) of user" :key="key">{{ key }}: {{ value }}</p>
             <img v-if="user.avatar" class="avatar" :src="user.avatar" />
           </div>
           <button class="btn btn-info" @click="mostrarPerfil = false">Esconder</button>
         </section>
         <form v-else class="row g-3 was-validated" @submit.prevent="salvarPerfil()" validate>
-          <div class="col-md-4">
+          <div>
             <label for="nameField" class="form-label">Nome</label>
             <input type="text" class="form-control" id="nameField" v-model="user.name" required />
-            <div class="invalid-feedback">Nome obrigatório</div>
           </div>
-          <div class="col-md-4">
+          <div>
             <label for="surnameField" class="form-label">Sobrenome</label>
             <input
               type="text"
@@ -92,12 +91,11 @@ function salvarPerfil() {
               v-model="user.surname"
               required
             />
-            <div class="invalid-feedback">Sobrenome obrigatório</div>
           </div>
-          <div class="col-md-4">
-            <label for="emailField" class="form-label">E-mail</label>
+          <div>
+            <label for="emailField" class="form-label">E-mail @</label>
             <div class="input-group">
-              <span class="input-group-text" id="emailFieldPrepend">@</span>
+              <span class="input-group-text" id="emailFieldPrepend"></span>
               <input
                 type="email"
                 class="form-control"
@@ -106,23 +104,21 @@ function salvarPerfil() {
                 v-model="user.email"
                 required
               />
-              <div class="invalid-feedback">E-mail obrigatório.</div>
-              <div class="valid-feedback">E-mail válido!</div>
             </div>
           </div>
-          <div class="col-md-2">
+          <div>
             <label for="senhaField" class="form-label">Senha</label>
             <input type="text" class="form-control" id="senhaField" v-model="user.senha" />
           </div>
-          <div class="col-md-2">
+          <div>
             <label for="senhaconfirmField" class="form-label">Confirmar senha</label>
             <input type="text" class="form-control" id="senhaconfirmField" v-model="user.senhaconfirm" />
           </div>
-          <div class="col-md-2">
+          <div>
             <label for="cityField" class="form-label">Cidade</label>
             <input type="text" class="form-control" id="cityField" v-model="user.city" />
           </div>
-          <div class="col-md-2">
+          <div>
             <label for="stateField" class="form-label">Estado</label>
             <select class="form-select" id="stateField" v-model="user.state">
               <option selected disabled value="">Selecionar...</option>
@@ -132,22 +128,21 @@ function salvarPerfil() {
             </select>
           </div>
           
-          <div class="col-md-2">
+          <div>
             <label for="ruaField" class="form-label">Rua:</label>
             <input type="text" class="form-control" id="ruaField" v-model="user.rua" />
           </div>
-          <div class="col-md-2">
+          <div>
             <label for="bairroField" class="form-label">Bairro</label>
             <input type="text" class="form-control" id="bairroField" v-model="user.bairro" />
           </div>
-          <div class="col-md-2">
+          <div>
             <label for="biografiaField" class="form-label">Biografia</label>
             <input type="text" class="form-control" id="biografiaField" v-model="user.biografia" />
           </div>
-          <div class="col-6">
-            <p class="mb-0">Hobbies</p>
+          <div >
+            <p>Hobbies</p>
             <input
-              class="ms-3 me-1"
               type="checkbox"
               id="hobbiesField"
               value="esportes"
@@ -155,7 +150,6 @@ function salvarPerfil() {
             />
             <label for="hobbiesField">Esportes</label>
             <input
-              class="ms-3 me-1"
               type="checkbox"
               id="hobbiesField"
               value="música"
@@ -163,7 +157,6 @@ function salvarPerfil() {
             />
             <label for="hobbiesField">Música</label>
             <input
-              class="ms-3 me-1"
               type="checkbox"
               id="hobbiesField"
               value="viagens"
@@ -171,18 +164,22 @@ function salvarPerfil() {
             />
             <label for="hobbiesField">Viagens</label>
             <input
-              class="ms-3 me-1"
               type="checkbox"
               id="hobbiesField"
               value="leitura"
               v-model="user.hobbies"
             />
             <label for="hobbiesField">Leitura</label>
-          </div>
-          <div class="col-6">
-            <p class="mb-0">Linguagem preferida</p>
             <input
-              class="ms-3 me-1"
+              type="checkbox"
+              id="hobbiesField"
+              value="leitura"
+              v-model="user.hobbies"
+            />
+          </div>
+          <div >
+            <p>Linguagem preferida</p>
+            <input
               type="radio"
               v-model="user.preferredLanguage"
               value="C"
@@ -190,7 +187,6 @@ function salvarPerfil() {
             />
             <label for="langC">C</label>
             <input
-              class="ms-3 me-1"
               type="radio"
               v-model="user.preferredLanguage"
               value="Java"
@@ -198,7 +194,6 @@ function salvarPerfil() {
             />
             <label for="langJava">Java</label>
             <input
-              class="ms-3 me-1"
               type="radio"
               v-model="user.preferredLanguage"
               value="Python"
@@ -206,7 +201,6 @@ function salvarPerfil() {
             />
             <label for="langPython">Python</label>
             <input
-              class="ms-3 me-1"
               type="radio"
               v-model="user.preferredLanguage"
               value="Javascript"
@@ -214,8 +208,8 @@ function salvarPerfil() {
             />
             <label for="langJs">JavaScript</label>
           </div>
-          <div class="col-12">
-            <button class="btn btn-primary" type="submit">Enviar</button>
+          <div>
+            <button  type="submit">Enviar</button>
           </div>
         </form>
       </transition>
@@ -228,12 +222,23 @@ function salvarPerfil() {
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap");
 
+.grid {
+    display: grid;
+    grid-template-columns: 3% 1fr;
+    grid-template-rows: 10vh 80vh 10vh;
+    grid-template-areas: "h h"
+        "m m"
+        "f f";
+}
+
 .container {
+  height: auto;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  background-color: #ddded9;
+  background-image: url("src/fundo.avif");
+  background-size: 100%;
 }
 
 .title {
@@ -244,22 +249,34 @@ function salvarPerfil() {
 h1 {
   font-family: "Work sans";
   font-weight: bold;
+  color: aliceblue;
 }
 
-.form {
+p{
+  display: flex;
+  align-self: flex-start;
+  font-family: "Work sans";
+  margin: 0px 0px 12px 13px;
+  font-size: 23px;
+  font-weight: bold;
+}
+
+form {
   font-family: "Work sans";
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   width: fit-content;
-  width: 350px;
+  width: 550px;
   padding: 40px;
-  background-color: rgb(250, 250, 250);
+  background-color: rgba(20, 18, 18, 0.527);
   border-radius: 30px;
   margin: 10px 50px 10px 50px;
   box-shadow: 1px 1px 10px 1px rgba(0, 0, 0, 0.3);
-  /* offset-x, offset-y, blur-radius, spread-radius */
+  color: aliceblue;
+  display: grid;
+  grid-template-areas: "main";
 }
 
 .inputs {
@@ -274,8 +291,7 @@ input {
   border-radius: 10px;
   border-color: #bab3ab;
   font-family: "Work sans";
-  margin: 2px 10px 15px 5px;
-  /*Top, right, bottom, left*/
+  margin-top: 0;
   padding: 0px 0px 0px 10px;
 }
 
@@ -283,8 +299,9 @@ label {
   display: flex;
   align-self: flex-start;
   font-family: "Work sans";
-  margin: 0px 0px 0px 13px;
+  margin-bottom: 10px;
   font-size: 1rem;
+  font-weight: bold;
 }
 
 select {
@@ -353,18 +370,20 @@ li {
   align-self: flex-start;
 }
 
-.del-button {
+button {
   margin: 0 30px;
   border: 0;
   height: fit-content;
   width: fit-content;
   cursor: pointer;
   align-self: end;
+  font-family: "Work sans";
   font-size: 1rem;
   transition: 100ms ease-in-out;
+  font-weight: bold;
 }
 
-.del-button:hover {
+button:hover {
   transform: scale(1.2);
   transition: 100ms ease-in-out;
   color: rgb(160, 0, 0);
@@ -375,12 +394,12 @@ textarea {
   border-color: #bab3ab;
   font-family: "Work sans";
   margin: 2px 10px 15px 10px;
-  /*Top, right, bottom, left*/
   padding: 5px 0px 0px 5px;
 }
 
 #results-form > p {
   margin: 10px 0 0 0;
+  word-break: normal;
 }
 
 .result-item {
